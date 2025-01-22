93872291aa84373fe2fbcf1a7fc297ef461ef568
Wong Zheng Yew <wong_zhengyew@mymail.sutd.edu.sg>
2025-01-22 18:08:00 +0800

    edited main.py with markdown

--- main.py
+++ main.py
@@ -11,3 +11,23 @@ st.markdown('<style>div.block-container{padding-top:3rem;}</style>', unsafe_allo
 
 # File uploader
 fl = st.file_uploader(":file_folder: Upload a file", type=["csv", "xls"])
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
