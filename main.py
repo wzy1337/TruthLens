8d06e50f71ce91e13ee0897703b206e04ae93963
Wong Zheng Yew <wong_zhengyew@mymail.sutd.edu.sg>
2025-01-22 17:23:08 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Page configuration
+st.set_page_config(page_title="Superstore!!", page_icon=":bar_chart:", layout="wide")
+
+# Title and styling
+st.title(":bar_chart: Sample Superstore EDA")
+st.markdown('<style>div.block-container{padding-top:3rem;}</style>', unsafe_allow_html=True)
+
+# File uploader
+fl = st.file_uploader(":file_folder: Upload a file", type=["csv", "xls"])
