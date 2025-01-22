13fc8865fa23d8bb62846e81a4b97a1d67a5938b
Wong Zheng Yew <wong_zhengyew@mymail.sutd.edu.sg>
2025-01-22 17:42:22 +0800

    initial commit of main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import plotly.express as px
 st.set_page_config(page_title="Superstore!!", page_icon=":bar_chart:", layout="wide")
 
 # Title and styling
-st.title(":bar_chart: Sample Superstore EDA")
+st.title(":bar_chart: Lenstruth")
 st.markdown('<style>div.block-container{padding-top:3rem;}</style>', unsafe_allow_html=True)
 
 # File uploader
